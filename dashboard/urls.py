9581296653084a61ad82a309e2dcaec77946a758
swati Jindal <swatijindal2018@gmail.com>
2021-06-04 21:21:10 +0530

    notesWork

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home)
-    
+    path('',views.home,name="home"),
+    path('notes',views.notes,name="notes"),
+    path('delete_note/<int:pk>',views.delete_note,name="delete-note"),
+
 ]
