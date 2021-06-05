37475ff95b9d784602564eccc6bffed14b95e353
swati Jindal <swatijindal2018@gmail.com>
2021-06-05 14:40:10 +0530

    commit

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from . forms import *
 from django.contrib import messages
 from django.views import generic
+from youtubesearchpython import VedioSearch
 
 # Create your views here.
 def home(request):
@@ -31,7 +32,34 @@ class NotesDetailView(generic.DetailView):
 
 
 def homework(request):
-    form = HomeworkForm()
+    if request.method == "POST":
+        form = HomeworkForm(request.POST)
+        if form.is_valid():
+    # exceptional handling
+            try: 
+                finished = request.POST['is_finished']
+                if finished == 'on' :
+                    finished = True
+                else:
+                    finished = False
+            except:
+                finished = False
+
+            homeworks = Homework(
+                user = request.user,
+                subject = request.POST['subject'],
+                title = request.POST['title'],
+                description = request.POST['description'],
+                due = request.POST['due'],
+                is_finished =finished
+
+            )
+            homeworks.save()
+            messages.success(request,f'Homework Added from {request.user.username} !!')
+           
+    else: 
+        form = HomeworkForm()
+
     homework = Homework.objects.filter(user=request.user)
     if len(homework) == 0:
         homework_done = True
@@ -39,9 +67,42 @@ def homework(request):
         homework_done = False
 
     context = {'homeworks':homework,
-    'homeworks_done':homework_done,
-    'form':form
+            'homeworks_done':homework_done,
+            'form':form
     }
     return render(request,'dashboard/homework.html',context)
+
+def update_homework(request,pk=None):
+    homework = Homework.objects.get(id=pk)
+    if homework.is_finished == True:
+        homework.is_finished = False
+    else:
+        homework.is_finished = True
+    homework.save()
+    return redirect('homework')
+
+def delete_homework(request,pk=None):
+    Homework.objects.get(id=pk).delete()
+    return redirect("homework")
+
+
+def youtube(request):
+    if request.method == "POST":
+        form = DashboardForm(request.POST)
+        text = request.POST['text']
+        vedio = VedioSearch(text,limit=10)
+        result_list = []
+        for i in vedio.result()['result']:
+            result_dic = {
+                'input':text,
+                'title':i['title'],
+            }
+    else:
+        form = DashboardForm()
+    context = {'form':form}
+    return render(request,"dashboard/youtube.html",context)
+
+
+
 
     
